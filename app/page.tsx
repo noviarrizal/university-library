e164562c45274dadcba8cb8102c8caac6c0b255a
arif noviarrizal <muhammad.noviarrizal@ist.id>
2025-01-12 12:48:30 +0700

    feat(taiwindconfig, fonts, favicon): adding tailwind config, fonts, and favicon

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
+const Home = () => {
   return (
     <>
       <Button>Click Me</Button>
     </>
   );
-}
+};
+
+export default Home;
